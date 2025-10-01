f88943427a636bfdbc2587e5ed887736a02ea042
edelryan <edelniriain@gmail.com>
2025-10-01 22:58:39 +0100

    Update gifteded.js

--- gifteded.js
+++ gifteded.js
@@ -1,65 +1,60 @@
 define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
-	
-	var API = new APIConstructor();
-		  return stiatExtension({
-		  category : { 
-		    name : 'Gifted Education', //Will appear in the data.
-		    title : {
-		      media : {word : 'Gifted Education'}, //Name of the category presented in the task.
-		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-		      height : 7 //Used to position the "Or" in the combined block.
-		    }, 
-		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
-		    ], 
-		    //Stimulus css (style)
-		    css : {color:'#31b404','font-size':'3em'}
-		  },	
 
-  		attribute1 : 
-			{
-			name : 'Not Valued', //Attribute label
-			title : {
-				media : {word : 'Not Valued'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Unimportant'},
-				{word: 'Unnecessary'},
-				{word: 'Wasteful'},
-				{word: 'Unfair'},
-				{word: 'Excessive'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
-			},
-		attribute2 : 
-			{
-			name : 'Valued', //Attribute label
-			title : {
-				media : {word : 'Valued'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Important'},
-				{word: 'Necessary'},
-				{word: 'Worthwhile'},
-				{word: 'Beneficial'},
-				{word: 'Needed'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
-			},
+  var API = new APIConstructor();
+  return stiatExtension({
+    category : { 
+      name : 'Gifted Education', // Will appear in the data.
+      title : {
+        media : {word : 'Gifted Education'}, // Name of the category presented in the task.
+        css : {color:'#31b404','font-size':'2em'},
+        height : 7
+      }, 
+      // Use word stimuli instead of images
+      media : [
+        {word : 'Gifted'},
+        {word : 'Talented'},
+        {word : 'High Ability'},
+        {word : 'Advanced'},
+        {word : 'Exceptional'},
+        {word : 'Bright'}
+      ],
+      css : {color:'#31b404','font-size':'3em'}
+    },
 
-  base_url : {//Where are your images at?
-    image : 'https://baranan.github.io/minno-tasks/images/'
-  }}
-  );
+    attribute1 : {
+      name : 'Not Valued',
+      title : {
+        media : {word : 'Not Valued'},
+        css : {color:'#31b404','font-size':'2em'},
+        height : 7
+      }, 
+      media : [
+        {word: 'Unimportant'},
+        {word: 'Unnecessary'},
+        {word: 'Wasteful'},
+        {word: 'Unfair'},
+        {word: 'Excessive'}
+      ],
+      css : {color:'#31b404','font-size':'3em'}
+    },
+
+    attribute2 : {
+      name : 'Valued',
+      title : {
+        media : {word : 'Valued'},
+        css : {color:'#31b404','font-size':'2em'},
+        height : 7
+      }, 
+      media : [
+        {word: 'Important'},
+        {word: 'Necessary'},
+        {word: 'Worthwhile'},
+        {word: 'Beneficial'},
+        {word: 'Needed'}
+      ],
+      css : {color:'#31b404','font-size':'3em'}
+    }
+
+    // No base_url needed since there are no images
   });
+});
